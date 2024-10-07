3de0ee7b4b58effa6b0955d981b2e5eb0f904853
Atledd <artem.gonzha@gmail.com>
2024-10-07 20:50:43 +0300

    revision 1

--- 02.py
+++ 02.py
@@ -1,11 +1,14 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    if min >= 1 and max <= 1000:
-        chosen_numbers = random.sample(range(min, max), quantity)
-        return f"Лотерейні числа: {chosen_numbers}"
-    else:
-        return "Wrong parameters! Please, make sure your parameters are between 1 and 1000."
+    try:
+        if min >= 1 and max <= 1000:
+            chosen_numbers = random.sample(range(min, max), quantity)
+            return f"Лотерейні числа: {chosen_numbers}"
+        else:
+            return "Wrong parameters! Please, make sure your parameters are between 1 and 1000."
+    except ValueError:
+        return "Please, make sure that all parameters are correct!"
 
 res = get_numbers_ticket(1, 49, 6)
 
